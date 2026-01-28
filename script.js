d7a89404c467db598d2e9ae8c717b675fb13f078
CrueUs429 <crueus429@gmail.com>
2026-01-28 17:32:41 +0900

    프로젝트 파일 전체 업데이트 및 재구성

---
+++ script.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const header = document.querySelector('header');
+    header.addEventListener('click', () => {
+        alert('스피키 밈은 정말 재미있어요!');
+    });
+});
